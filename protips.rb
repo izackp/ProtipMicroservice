89232124c6814665ed01f28f1729aea9b241f978
Isaac Paul <izackp@gmail.com>
2015-03-17 13:21:30 -0400

    initial commit

---
+++ protips.rb
@@ -0,0 +1,24 @@
+PROTIPS = [
+  "Nuke the whales.",
+  "Node.js is a performant solution to the problem you are solving.",
+  "Don't stop, never give up. Hold your head high and reach the top.",
+  "Tupac is not dead.",
+  "Emoji are totally acceptable as variable names in unicode languages.",
+  "Sharks.",
+  "More like, utf-great!",
+  "The two hardest problems in computer science is cache invalidation and when to get food.",
+  "Doing ten jumping jacks will make you look like an idiot.",
+  "Use the phrase My understanding was... instead of I assumed...",
+  "When copying text, use Control+Shift+V to paste without formatting",
+  "Instead of saying yeah you told me when someone repeats a story, say yes, I remember this.",
+  "Keep your drugs tired to a helium balloon at all times. If you get stopped by the police, just let them float away.",
+  "If you see someone crying, ask if it's because of their haircut.",
+  "Wife pulls a knife on you during an argument? Grab the bread and mayo, her womanly instincts will kick in.",
+  "If you happen to find yourself on To Catch a Predator start singing Happy Birthday to You. The network won't be able to air it without paying exorbitant licensing fees.",
+  "If a police dog is chasing you, try not to dive in a tunnel, then on to a seesaw, then jump through a hoop of fire. They're trained for that.",
+  "Need to look up something on the internet? Use Bing.com!",
+  "Cold? Stand in a corner! All corners are 90 degrees!",
+  "Instead of saying I know say I understand"
+]
+
+puts "{\"protip\": \"#{PROTIPS.sample}\"}"\
